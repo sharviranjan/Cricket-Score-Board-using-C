2d65124aedf7a59b063ae94d318b534229e9a6e9
sharviranjan <122813453+sharviranjan@users.noreply.github.com>
2024-09-05 16:45:04 +0530

    Add files via upload

---
+++ Cricket_scoreboard.c
@@ -0,0 +1,381 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+void first_inning(char y[5]);
+int second_inning(char y[5], char x[5]);
+
+char Team1[5];
+char Team2[5];
+int toss, target, overs, score, wickets, inning = 1;
+float runrate;
+int balls, p_balls;
+float c_overs;
+int bat_first;
+
+struct Player
+{
+    char name[50];
+    int runs;
+    struct Player *next;
+};
+
+struct ScoreBoard
+{
+    struct Player *head;
+    int totalRuns;
+    int totalWickets;
+    float overs;
+};
+
+struct Player *createPlayer(char playerName[])
+{
+    struct Player *newPlayer = (struct Player *)malloc(sizeof(struct Player));
+    if (newPlayer != NULL)
+    {
+        strcpy(newPlayer->name, playerName);
+        newPlayer->runs = 0;
+        newPlayer->next = NULL;
+    }
+    return newPlayer;
+}
+
+void insertPlayer(struct Player **head, char playerName[])
+{
+    struct Player *newPlayer = createPlayer(playerName);
+    if (*head == NULL)
+    {
+        *head = newPlayer;
+    }
+    else
+    {
+        struct Player *temp = *head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newPlayer;
+    }
+}
+
+void displayScoreBoard(struct ScoreBoard *board)
+{
+    printf("\n------------------- Score Board -------------------\n");
+    printf("Total Runs: %d\n", board->totalRuns);
+    printf("Total Wickets: %d\n", board->totalWickets);
+    printf("Overs: %.1f\n", board->overs);
+
+    struct Player *current = board->head;
+    printf("\nPlayers\t\tRuns\n");
+    while (current != NULL)
+    {
+        printf("%s\t\t%d\n", current->name, current->runs);
+        current = current->next;
+    }
+    printf("---------------------------------------------------\n");
+}
+
+void updateScore(struct ScoreBoard *board, char playerName[], int runs)
+{
+    struct Player *current = board->head;
+    while (current != NULL)
+    {
+        if (strcmp(current->name, playerName) == 0)
+        {
+            current->runs += runs;
+            board->totalRuns += runs;
+            return;
+        }
+        current = current->next;
+    }
+}
+
+int main2();
+
+
+
+//=====================================================================================================================
+
+int main()
+{
+
+    printf("*-------------------------------------------*");
+    printf("\n \t\tBeast Cricket Scorer\t\t");
+    printf("*\n-------------------------------------------*");
+    printf("\nPress any key to Continue");
+    getch();
+    system("cls");
+
+    printf("Enter Name of Team1:");
+    scanf("%s", Team1);
+    printf("Enter Name of Team2:");
+    scanf("%s", Team2);
+    printf("Enter the number of overs:");
+    scanf("%d", &overs);
+    balls = overs * 6;
+re:
+    printf("Which Team is batting first?\n");
+    printf("\n[1] %s\n[2] %s\n", Team1, Team2);
+    scanf("%d", &toss);
+    switch (toss)
+    {
+    case 1:
+        printf("Team 1 is batting first");
+        first_inning(Team1);
+        bat_first = 1;
+        break;
+    case 2:
+        printf("Team 2 is batting first");
+        first_inning(Team2);
+        bat_first = 2;
+
+        break;
+    default:
+        printf("Enter a valid choice!");
+        goto re;
+    }
+    if (target > 0)
+    {
+        if (bat_first == 1)
+        {
+            second_inning(Team2, Team1);
+        }
+        else
+        {
+            second_inning(Team1, Team2);
+        }
+    }
+
+    return 0;
+}
+
+void first_inning(char y[5])
+{
+    p_balls = 0;
+    score = 0, wickets = 0;
+    int j;
+    int scorer, r;
+    char bt1[10];
+    printf(":--------------First Innings----------------\n");
+
+    for (j = 0; j != balls; j++)
+    {
+    label:
+        printf("Last ball...");
+        printf("\n[1]Normal delivery\n[2]No ball/wide\n");
+        scanf("%d", &scorer);
+        printf("\nRuns on last ball\n");
+        printf("[0]Dot ball\n[1]1 run\n[2]2 runs\n[3]3 runs\n[4]4 \n[5]6\n[6]Wicket!\n");
+        scanf("%d", &r);
+        switch (scorer)
+        {
+        case 1:
+            p_balls += 1;
+            if (p_balls % 6 == 0)
+            {
+                c_overs += 0.5;
+            }
+            else
+            {
+                c_overs += 0.1;
+            }
+            break;
+        case 2:
+            score += 1;
+        default:
+            goto label;
+            break;
+        }
+        switch (r)
+        {
+        case 0:
+            break;
+        case 1:
+            score += 1;
+            break;
+        case 2:
+            score += 2;
+            break;
+        case 3:
+            score += 3;
+            break;
+        case 4:
+            score += 4;
+            break;
+        case 5:
+            score += 6;
+            break;
+        case 6:
+            wickets += 1;
+            break;
+
+        default:
+            printf("Enter a valid choice!");
+            goto label;
+            break;
+        }
+        runrate = score / overs;
+        printf(":--------------First Innings----------------\n");
+        printf("Inning 1|");
+        printf("\nSCORE---%s|%d-%d|\t\t |overs:- %.1f|", y, score, wickets, c_overs);
+        printf("\n RR:-%.2f", runrate);
+
+        getch();
+        system("cls");
+        if (wickets == 10 || p_balls == balls)
+        {
+            inning = 2;
+            if (inning == 2)
+            {
+                target = score + 1;
+                printf("target is %d", target);
+            }
+            break;
+        }
+    }
+}
+int second_inning(char y[5], char x[5])
+{
+    p_balls = 0;
+    c_overs = 0;
+    score = 0, wickets = 0;
+    int j;
+    int scorer, r;
+    printf("\n:--------------SECOND Innings----------------\n");
+    for (j = 0; j != balls; j++)
+    {
+    label:
+        printf("Last ball...\n");
+        printf("\n[1]Normal delivery\n[2]No ball/wide\n");
+        scanf("%d", &scorer);
+        printf("\nRuns on last ball\n");
+        printf("[0]Dot ball\n[1]1 run\n[2]2 runs\n[3]3 runs\n[4]4 \n[5]6\n[6]Wicket!\n");
+        scanf("%d", &r);
+        switch (scorer)
+        {
+        case 1:
+            p_balls += 1;
+            if (p_balls % 6 == 0)
+            {
+                c_overs += 0.5;
+            }
+            else
+            {
+                c_overs += 0.1;
+            }
+            break;
+        case 2:
+            score += 1;
+        default:
+            break;
+        }
+        switch (r)
+        {
+        case 0:
+            break;
+        case 1:
+            score += 1;
+            break;
+        case 2:
+            score += 2;
+            break;
+        case 3:
+            score += 3;
+            break;
+        case 4:
+            score += 4;
+            break;
+        case 5:
+            score += 6;
+            break;
+        case 6:
+            wickets += 1;
+            break;
+
+        default:
+            printf("Enter a valid choice!");
+            goto label;
+            break;
+        }
+
+        runrate = score / overs;
+
+        printf("\n:--------------SECOND innings----------------\n");
+
+        printf("Inning 2|");
+        printf("\nSCORE---%s|%d-%d|\t\t |overs:- %.1f| Target - %d|", y, score, wickets, c_overs, target);
+        printf("\n RR:-%.2f", runrate);
+
+        if (score <= target)
+        {
+            printf("|Need %d of %d to win| ", target - score, balls - p_balls);
+        }
+
+        getch();
+        system("cls");
+        if (wickets == 10 || p_balls == balls || score >= target)
+        {
+            main2();
+            if (score >= target)
+            {
+                printf("%s Won the Match!", y);
+                return 0;
+            }
+            else
+            {
+                printf("%s Won the Match!", x);
+            }
+        }
+    }
+}
+
+
+
+int main2()
+{
+    struct ScoreBoard board;
+    board.head = NULL;
+    board.totalRuns = 0;
+    board.totalWickets = 0;
+    board.overs = 0.0;
+
+    int choice;
+    char playerName[50];
+    int runs;
+
+    do
+    {
+        printf("\n1. Add Player\n");
+        printf("2. Update Score\n");
+        printf("3. Display Score Board\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter player name: ");
+            scanf("%s", playerName);
+            insertPlayer(&board.head, playerName);
+            break;
+        case 2:
+            printf("Enter player name: ");
+            scanf("%s", playerName);
+            printf("Enter runs scored: ");
+            scanf("%d", &runs);
+            updateScore(&board, playerName, runs);
+            break;
+        case 3:
+            displayScoreBoard(&board);
+            break;
+        case 4:
+            printf("Exiting the program.\n");
+            break;
+        default:
+            printf("Invalid choice. Please enter a valid option.\n");
+        }
+    } while (choice != 4);
+
+    return 0;
+}
